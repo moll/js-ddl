7d6a506a6ad433d2cb89d3a4b49dd85ac5d0b523
Andri Möll <andri@dot.ee>
2013-10-01 17:54:31 +0300

    Export only a single function.

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-exports.__defineGetter__("Sqlite3", function() {
+exports.__defineGetter__("sqlite3", function() {
   return require("./lib/sqlite3")
 })
 
-exports.__defineGetter__("sqlite3", function() {
-  return exports.Sqlite3.attributes
-})
-
-exports.__defineGetter__("PostgreSQL", function() {
-  return require("./lib/postgresql")
-})
-
 exports.__defineGetter__("postgresql", function() {
-  return exports.PostgreSQL.attributes
+  return require("./lib/postgresql")
 })
